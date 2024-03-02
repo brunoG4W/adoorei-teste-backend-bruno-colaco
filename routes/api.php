892f602894f9ad2a1a031a870632a531c42297ad
Bruno Colaço <brunowow@gmail.com>
2024-03-02 13:25:04 -0300

    feat(api):name routes

--- routes/api.php
+++ routes/api.php
@@ -7,19 +7,19 @@ use App\Http\Controllers\API\ProductsController;
 use App\Http\Controllers\API\SalesController;
 
 //Listar produtos disponíveis
-Route::get('products', [ProductsController::class, 'listProducts']);
+Route::get('products', [ProductsController::class, 'listProducts'])->name('api.products.list');
 
 //Cadastrar nova venda
-Route::post('sale', [SalesController::class, 'addSale']);
+Route::post('sale', [SalesController::class, 'addSale'])->name('api.sales.create');
 
 //Consultar vendas realizadas
-Route::get('sales', [SalesController::class, 'listSales']);
+Route::get('sales', [SalesController::class, 'listSales'])->name('api.sales.list');
 
 //Consultar uma venda específica
-Route::get('sales/{id}', [SalesController::class, 'getSale']);
+Route::get('sales/{id}', [SalesController::class, 'getSale'])->name('api.sales.detail');
 
 //Cancelar uma venda
-Route::post('sale/{id}/cancel', [SalesController::class, 'cancelSale']);
+Route::post('sale/{id}/cancel', [SalesController::class, 'cancelSale'])->name('api.sales.cancel');
 
 //Cadastrar novas produtos a uma venda]
-Route::post('sale/{id}/add_products', [SalesController::class, 'addProductsToSale']);
+Route::post('sale/{id}/add_products', [SalesController::class, 'addProductsToSale'])->name('api.sales.products.add');
