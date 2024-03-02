cbb2a674d1e98efece7ae1259d4a5df28895c6e2
Bruno Colaço <brunowow@gmail.com>
2024-03-02 14:29:18 -0300

    feat(product):create product model, factory, test

--- app/Http/Controllers/API/ProductsController.php
+++ app/Http/Controllers/API/ProductsController.php
@@ -6,11 +6,21 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+use App\Models\Product;
+
 class ProductsController extends Controller
 {
+
+    public function __construct(private Product $product)
+    {
+    }
+
+
+
     public function listProducts() : Response 
     {     
-        return response()->json(['ok'], 200);
+        return response()->json($this->product->all());
+        
     }
 }
 
--- tests/Feature/API/ProductsTest.php
+++ tests/Feature/API/ProductsTest.php
@@ -5,14 +5,35 @@ namespace Tests\Feature\API;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Testing\Fluent\AssertableJson;
+
+use App\Models\Product;
 
 class ProductsTest extends TestCase
 {
+    use RefreshDatabase;
 
     public function test_get_products_endpoint(): void
     {
+        $products = Product::factory(3)->create();        
         $response = $this->get(route('api.products.list'));
+
         $response->assertStatus(200);
+        $response->assertJsonCount(3);
+
+        $response->assertJson(function (AssertableJson $json) use($products){
+            $json->whereAllType([
+                '0.id' => 'integer',
+                '0.name' => 'string',                 
+                '0.description'=> 'string'
+            ]);
+
+            $json->hasAll(['0.id', '0.name', '0.price', '0.description']);
+
+        });
+
+
+
     }
 
 }
